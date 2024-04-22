47b7ccee9c470627703027b2bbe1a50856a9aa76
zxk <775275493@qq.com>
2024-04-22 19:55:04 +0800

    完成标准化代码

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 A simple implement of the DDPM model with cifar10
 - [x] 完成预测噪声网络
 - [x] 完成DDPM前向和反向过程代码
-- [ ] 完成DDPM损失函数
-- [ ] 完成DDPM生成cifar10样本
-- [ ] 标准化代码
+- [X] 完成DDPM损失函数
+- [X] 完成DDPM生成cifar10样本
+- [X] 标准化代码
+- [ ] 理论笔记
--- ddpm.py
+++ ddpm.py
@@ -1,96 +1,105 @@
 import math
+import os
 import time
 import numpy as np
 import torch
 import torch.nn as nn
+
 import torch.nn.functional as F
-
-
-class MLP(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super(MLP, self).__init__()
-        self.pos = nn.Linear(input_dim, hidden_dim)
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_dim, output_dim)
-
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_uniform_(m.weight)
-                nn.init.zeros_(m.bias)
-
-    def forward(self, x, t):
-        x = self.fc1(torch.cat([x, t], dim=1))
-        x = self.relu1(x)
-        x = self.fc2(x)
-        return x
+from unet import NaiveUnet
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+from torchvision.utils import make_grid, save_image
+from tqdm import tqdm
 
 
 class Diffusion(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim, T, *args, **kwargs):
+    def __init__(self, in_channels, out_channels, betas, T, device):
         super(Diffusion, self).__init__()
-        self.model = MLP(input_dim * 2, hidden_dim, output_dim)
+        self.model = NaiveUnet(in_channels, out_channels, n_feat=128)
         self.T = T
-        self.device = kwargs['device']
-        self.betas = torch.linspace(
-            0.0001, 0.02, self.T, dtype=torch.float32, device=self.device
-        )
-        self.alphas = 1 - self.betas
-        self.alphas_bar = torch.cumprod(self.alphas, 0)
-        self.betas_bar = 1 - self.alphas_bar
-        self.sqrt_alphas_bar = torch.sqrt(self.alphas_bar)
-        self.sqrt_one_minus_alphas_bar = torch.sqrt(1 - self.alphas_bar)
-        self.sqrt_recip_alphas_bar = torch.sqrt(1 / self.alphas_bar)
-        self.sqrt_alphas = torch.sqrt(self.alphas)
-        self.alphas_bar_prev = torch.cat([torch.ones(1, device=kwargs["device"]), self.alphas_bar[:-1]])
-        self.one_minus_alphas_bar_prev = 1.0 - self.alphas_bar_prev
-        self.sqrt_alphas_bar_prev = torch.sqrt(self.alphas_bar_prev)
-        self.recip_one_minus_alphas_bar = 1.0 / self.one_minus_alphas_bar
+        self.device = device
+        for k, v in ddpm_schedules(*betas, T).items():
+            self.register_buffer(k, v)
+        self.loss_fn = nn.MSELoss()
 
-    # 加噪过程
-    def q_sample(self, x0, t, noise=None):
-        if noise == None:
-            noise = torch.randn_like(x0)
-        return self.sqrt_alphas_bar[t] * x0 + self.sqrt_one_minus_alphas_bar[t] * noise
+    def forward(self, x0):
+        t = torch.randint(1, self.T + 1, size=(x.shape[0],), device=device)
+        noise = torch.randn_like(x0, device=device)
+        xt = self.sqrt_alphas_bar[t, None, None, None] * x0 + self.sqrt_one_minus_alphas_bar[
+            t, None, None, None] * noise
+        return self.loss_fn(noise, self.model(xt, t / self.T))
 
-    # 采样过程
+    def sample(self, n_sample, shape, device):
+        xt = torch.randn(n_sample, *shape).to(device)
+        for t in range(self.T, 0):
+            z = torch.randn(n_sample, *shape).to(device) if t > 1 else 0
+            xt = self.sqrt_recip_alphas[0] * \
+                 (xt - self.betas[t] /
+                  self.sqrt_one_minus_alphas_bar[t] * self.model(xt, t / self.T)) + \
+                 self.betas[t] * z
+        return xt
 
-    # 用xt预测x0
-    def get_x0_from_xt(self, xt, t, pred_noise):
-        return self.sqrt_recip_alphas_bar[t] * xt - self.sqrt_one_minus_alphas_bar[t] * pred_noise
 
-    def q_posterior(self, x0, xt, t):
-        posterior_mean = (self.sqrt_alphas[t] * self.one_minus_alphas_bar_prev[t] * xt + self.sqrt_alphas_bar_prev[t] *
-                          self.betas[t] * x0) * self.recip_one_minus_alphas_bar[t]
-        posterior_variance = self.betas[t] * self.one_minus_alphas_bar_prev[t] * self.recip_one_minus_alphas_bar[t]
-        posterior_log_variance = torch.log(posterior_variance)
+def ddpm_schedules(beta1: float, beta2: float, T: int):
+    betas = torch.linspace(beta1, beta2, T + 1, dtype=torch.float32)
+    alphas = 1 - betas
+    log_alphas = torch.log(alphas)
+    alphas_bar = torch.cumsum(log_alphas, dim=0).exp()
+    one_minus_alphas_bar = torch.sqrt(1 - alphas_bar)
+    sqrt_alphas = 1 / alphas
+    sqrt_alphas_bar = torch.sqrt(alphas_bar)
+    sqrt_one_minus_alphas_bar = torch.sqrt(one_minus_alphas_bar)
+    sqrt_recip_alphas = 1 / sqrt_alphas
+    return {'betas': betas,
+            'sqrt_alphas_bar': sqrt_alphas_bar,
+            'sqrt_one_minus_alphas_bar': sqrt_one_minus_alphas_bar,
+            'sqrt_recip_alphas': sqrt_recip_alphas,
+            }
 
-        return posterior_mean, posterior_variance, posterior_log_variance
 
-    def p_mean_variance(self, xt, t):
-        pred_noise = self.model(xt, t)
-        x_recon = self.get_x0_from_xt(xt, t, pred_noise)
-        x_recon.clamp_(-1, 1)
-        model_mean, posterior_variance, posterior_log_variance = self.q_posterior(
-            x_recon, xt, t
-        )
-        return model_mean, posterior_variance, posterior_log_variance
+if __name__ == '__main__':
+    device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
+    ddpm = Diffusion(3, 3, (0.0001, 0.02), 1000, 'cuda:1')
+    ddpm.to(device)
+    tf = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
 
-    def p_sample(self, xt, t, noise=None):
-        b = xt[0]
-        if noise == None:
-            noise = torch.randn_like(xt)
-        posterior_mean, posterior_variance, posterior_log_variance = self.p_mean_variance(xt, t)
-        # 得到的是b*其他维度都为1的shape
-        # 当t不为0的时候 这个mask为1，否则为0 目的是t=0的时候不引入噪声
-        mask = (1 - (t == 0)).float().reshape(b, *((1,) * (len(xt) - 1)))
-        return posterior_mean + mask * torch.exp(0.5 * posterior_log_variance) * noise
+    dataset = CIFAR10(
+        "/mnt/data0/public_datasets/torch_build_in_datas",
+        train=True,
+        download=True,
+        transform=tf,
+    )
+    dataloader = DataLoader(dataset, batch_size=512, shuffle=True, num_workers=16)
+    optim = torch.optim.Adam(ddpm.parameters(), lr=1e-5)
+    n_epoch = 100
+    for i in range(n_epoch):
+        print(f"Epoch {i} : ")
+        ddpm.train()
 
-    def p_sample_loop(self, device, shape):
-        batch_size = shape[0]
-        x = torch.randn(shape, device=device, requires_grad=False)
-        for i in range(self.T - 1, -1, -1):
-            t = torch.full((batch_size,), i, device=device, dtype=torch.long)
-            x = self.p_sample(x, t)
-        return x
+        pbar = tqdm(dataloader)
+        loss_ema = None
+        for x, _ in pbar:
+            optim.zero_grad()
+            x = x.to(device)
+            loss = ddpm(x)
+            loss.backward()
+            if loss_ema is None:
+                loss_ema = loss.item()
+            else:
+                loss_ema = 0.9 * loss_ema + 0.1 * loss.item()
+            pbar.set_description(f"loss: {loss_ema:.4f}")
+            optim.step()
+
+        ddpm.eval()
+        with torch.no_grad():
+            xh = ddpm.sample(8, (3, 32, 32), device)
+            xset = torch.cat([xh, x[:8]], dim=0)
+            grid = make_grid(xset, normalize=True, value_range=(-1, 1), nrow=4)
+            os.makedirs('./contents', exist_ok=True)
+            save_image(grid, f"./contents/ddpm_sample_cifar{i}.png")
+            # save model
+            torch.save(ddpm.state_dict(), f"./ddpm_cifar.pth")
